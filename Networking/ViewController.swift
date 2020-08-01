1736d55389311e9fe4ffc2197b3868e0bc2d7964
alexandr_sadunenko <a.sadunenko@gmail.com>
2020-08-01 13:17:33 +0300

    Initial Commit

---
+++ Networking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Networking
+//
+//  Created by Alex on 01.08.2020.
+//  Copyright © 2020 Alex Sadunenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
